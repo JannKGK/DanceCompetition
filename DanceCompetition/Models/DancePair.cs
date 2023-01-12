fc256f30bf81ad9277c29161d6ce5183f55c333a
JannKGK <jannkgk@gmail.com>
2023-01-12 12:10:57 +0200

    Add data annotation and name field to DancePair model.

--- DanceCompetition/Models/DancePair.cs
+++ DanceCompetition/Models/DancePair.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DanceCompetition.Models
 {
     public class DancePair
     {
         public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 100 characters.")]
+        public string Name { get; set; }
+
+        [Range(0, 5, ErrorMessage = "Grade must be between 0 and 5.")]
         public int grade1 { get; set; }
+
+        [Range(0, 5, ErrorMessage = "Grade must be between 0 and 5.")]
         public int grade2 { get; set; }
+
+        [Range(0, 5, ErrorMessage = "Grade must be between 0 and 5.")]
         public int grade3 { get; set; }
 
         public double getAverageGrade()
